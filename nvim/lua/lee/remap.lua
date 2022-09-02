aec05d8bdd33daa4618323932c8e75035d89deb4
Lee Aplin <lee@substrakt.com>
2022-09-02 22:28:23 +0100

    ADD: Telescope shortcut

--- nvim/lua/lee/remap.lua
+++ nvim/lua/lee/remap.lua
@@ -2,3 +2,4 @@ local nnoremap = require("lee.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("<C-f>", ":Telescope current_buffer_fuzzy_find <CR>")
+nnoremap("<C-p>", ":Telescope <CR>")
