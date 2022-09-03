56465e19b1f101cec3ff1d37e0691ef1647b8bb2
Lee Aplin <lee@substrakt.com>
2022-09-03 22:51:49 +0100

    ADD: Go to definition (php)

--- nvim/lua/lee/nativelsp.lua
+++ nvim/lua/lee/nativelsp.lua
@@ -8,6 +8,7 @@ require'lspconfig'.intelephense.setup{
     on_attach = function()
         -- In normal mode, capital K will enable hover information for the current buffer only
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
     end,
 }
 
