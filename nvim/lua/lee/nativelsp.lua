0fbde2ed3afda3e7dc7de17ff8acb877e8db5061
Lee Aplin <lee@substrakt.com>
2022-09-03 22:46:35 +0100

    ADD: Buffer hover information

--- nvim/lua/lee/nativelsp.lua
+++ nvim/lua/lee/nativelsp.lua
@@ -4,7 +4,12 @@
 require'lspconfig'.pylsp.setup{}
 
 -- PHP - intelephense https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#intelephense
-require'lspconfig'.intelephense.setup{}
+require'lspconfig'.intelephense.setup{
+    on_attach = function()
+        -- In normal mode, capital K will enable hover information for the current buffer only
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    end,
+}
 
 -- Terraform - terraform_lsp https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#terraform_lsp
 require'lspconfig'.terraform_lsp.setup{}
