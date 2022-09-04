68acdac5cc6d20ea6ece2f2e85a044e3460edef1
Lee Aplin <lee@substrakt.com>
2022-09-04 08:25:32 +0100

    ADD: Rename keymap

--- nvim/lua/lee/nativelsp.lua
+++ nvim/lua/lee/nativelsp.lua
@@ -1,22 +1,31 @@
 -- Language Server Protocols
+local on_attach = function()
+    -- In normal mode, capital K will enable hover information for the current buffer only
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    -- Go to definition
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    -- Rename function names or variables in the current buffer
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+    -- Scroll forward through errors in the current file
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0}) 
+    -- Scroll backthrough errors in the current file
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
+    -- Open error diagnostics in Telescope as a list
+    vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics <CR>", {buffer=0})
+end
 
 -- Python - python-lsp-server https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pylsp 
-require'lspconfig'.pylsp.setup{}
+require'lspconfig'.pylsp.setup{
+    on_attach = on_attach, 
+}
 
 -- PHP - intelephense https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#intelephense
 require'lspconfig'.intelephense.setup{
-    on_attach = function()
-        -- In normal mode, capital K will enable hover information for the current buffer only
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-        -- Go to definition
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-        -- Scroll through errors in the current file
-        vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer=0})
-        -- Open error diagnostics in Telescope as a list
-        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics <CR>", {buffer=0})
-    end,
+    on_attach = on_attach,
 }
 
 -- Terraform - terraform_lsp https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#terraform_lsp
-require'lspconfig'.terraform_lsp.setup{}
+require'lspconfig'.terraform_lsp.setup{
+    on_attach = on_attach, 
+}
 
