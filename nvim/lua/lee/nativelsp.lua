7dcfab43c914f728bc6ee163ac12f2c4ab1864e5
Lee Aplin <lee@substrakt.com>
2022-11-17 15:26:28 +0000

    ADD: List definitions

--- nvim/lua/lee/nativelsp.lua
+++ nvim/lua/lee/nativelsp.lua
@@ -4,17 +4,19 @@ local on_attach = function()
     local opts = {buffer=0}
 
     -- In normal mode, capital K will enable hover information for the current buffer only
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     -- Go to definition
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) 
     -- Rename function names or variables in the current buffer
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
     -- Scroll forward through errors in the current file
-    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0}) 
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, opts) 
     -- Scroll backthrough errors in the current file
-    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, opts)
     -- Open error diagnostics in Telescope as a list
-    vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics <CR>", {buffer=0})
+    vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics <CR>", opts) 
+    -- Get definition
+    vim.keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_references <CR>", opts)
 end
 
 require("nvim-lsp-installer").setup{}
